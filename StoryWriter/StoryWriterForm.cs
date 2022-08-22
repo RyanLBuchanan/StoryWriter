6b77813bf5106a9d7c96502aa5865b7e066046b3
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-22 13:38:16 -0600

    Add project files.

---
+++ StoryWriter/StoryWriterForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoryWriter
+{
+    public partial class StoryWriterForm : Form
+    {
+        public StoryWriterForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
