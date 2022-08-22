eec5c56c138620fd49d79b3a01d40b5cb9f23c33
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-08-22 14:22:00 -0600

    feat: Added Word class with functional code

--- StoryWriter/StoryWriterForm.Designer.cs
+++ StoryWriter/StoryWriterForm.Designer.cs
@@ -28,36 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.generateStoryButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // generateStoryButton
             // 
-            this.button1.Location = new System.Drawing.Point(520, 496);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.generateStoryButton.Location = new System.Drawing.Point(160, 408);
+            this.generateStoryButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.generateStoryButton.Name = "generateStoryButton";
+            this.generateStoryButton.Size = new System.Drawing.Size(168, 40);
+            this.generateStoryButton.TabIndex = 0;
+            this.generateStoryButton.Text = "Generate Story";
+            this.generateStoryButton.UseVisualStyleBackColor = true;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(24, 24);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(440, 368);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
             // 
             // StoryWriterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 788);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.generateStoryButton);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "StoryWriterForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Story Writer";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button generateStoryButton;
+        private System.Windows.Forms.RichTextBox outputTextBox;
     }
 }
 
---
+++ StoryWriter/Words.cs
@@ -0,0 +1,84 @@
+﻿//(Random Sentences and Story Writer) Write an app that uses random-number generation to create
+//sentences. Use four arrays of strings, called article, noun, verb and preposition. Create a
+//sentence by selecting a word at random from each array in the following order:
+//article, noun, verb, preposition, article, noun.As each word is picked, concatenate it to
+//the previous words in the sentence. The words should be separated by spaces. When the sentence
+//is output, it should start with a capital letter and end with a period. The program should
+//generate 10 sentences and output them to a text box.
+
+//The arrays should be filled as follows: The article array should contain
+//the articles "the", "a", "one", "some" and "any";
+//the nouns "boy", "girl", "dog", "town" and "car";
+//the verbs "drove", "jumped", "ran", "walked" and "skipped";
+//the prepositions "to", "from", "over", "under" and "on";
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryWriter
+{
+    class Words
+    {
+        private Random rand;
+
+        // Constructor to initialize arrays with pre-defined words
+        public Words()
+        {
+            Article = new string[] { "the", "a", "one", "some", "any" };
+            Noun = new string[] { "boy", "girl", "dog", "town", "car" };
+            Verb = new string[] { "drove", "jumped", "ran", "walked","skipped" };
+            Preposition = new string[] { "to", "from", "over", "under","on" };
+        }
+
+        public readonly string[] Article;
+        public readonly string[] Noun;
+        public readonly string[] Verb;
+        public readonly string[] Preposition;
+
+        // Generates sentence from 6 words 
+        private string GenerateSentence()
+        {
+            string word1 = GetWord("Article");
+            word1 = Capitalize(word1);
+            string word2 = GetWord("Noun");
+            string word3 = GetWord("Verb");
+            string word4 = GetWord("Preposition");
+
+            return String.Format($"{word1} {word2} {word3} {word4} {word1} {word2}.");
+        }
+
+        // Get random word from pre-defined array 
+        private string GetWord(string arrayName)
+        {
+            switch (arrayName)
+            {
+                case "Article":
+                    return Article[rand.Next(0, 5)];
+                case "Noun":
+                    return Noun[rand.Next(0, 5)];
+                case "Verb":
+                    return Verb[rand.Next(0, 5)];
+                case "Preposition":
+                    return Preposition[rand.Next(0, 5)];
+                default:
+                    return "Please restart application";
+
+            }
+        }
+
+        // Capitalize the first letter of first word in each sentence
+        private string Capitalize(string word)
+        {
+            char firstLetter = ' ';
+            string capitalizedWord = "";
+
+            firstLetter = Convert.ToChar(word.Substring(0, 1));
+            capitalizedWord = firstLetter.ToString().ToUpper() + word.Substring(1);
+
+            return capitalizedWord;
+        }
+    }
+}
